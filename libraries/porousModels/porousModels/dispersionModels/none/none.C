14cf320b6a2147288094cd1be2ea41488c23d793
csoulain <cyprien.soulaine@gmail.com>
2020-04-06 16:50:40 +0200

    add dispersionModel none

---
+++ libraries/porousModels/porousModels/dispersionModels/none/none.C
@@ -0,0 +1,83 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "none.H"
+#include "addToRunTimeSelectionTable.H"
+
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace dispersionModels
+{
+    defineTypeNameAndDebug(none, 0);
+
+    addToRunTimeSelectionTable
+    (
+        dispersionModel,
+        none,
+        dictionary
+    );
+}
+}
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::dispersionModels::none::none
+(
+    const fvMesh& mesh,
+    const dictionary& dict
+)
+:
+    dispersionModel(mesh, dict),
+    Deff_
+     (
+      IOobject
+      (
+       "Deff",
+       mesh_.time().timeName(),
+       mesh_,
+       IOobject::READ_IF_PRESENT,
+       IOobject::AUTO_WRITE
+      ),
+      mesh_,
+      dimensionedScalar("Deff",dimensionSet(0, 2, -1, 0, 0),0.0),
+      "zeroGradient"
+    )
+{
+
+}
+
+// * * * * * * * * * * * * * * member functions  * * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField>
+Foam::dispersionModels::none::effectiveDispersion() const
+{
+      return Deff_;
+}
+
+void Foam::dispersionModels::none::updateDispersion()
+{}
+// -------------------------------------------------------------------------//
