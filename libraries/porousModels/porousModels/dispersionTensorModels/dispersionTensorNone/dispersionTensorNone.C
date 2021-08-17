db7fbf0901410daf0294fc0d0baecff770a01286
csoulain <cyprien.soulaine@gmail.com>
2021-08-17 11:44:19 +0200

    dispersionTensorNone

---
+++ libraries/porousModels/porousModels/dispersionTensorModels/dispersionTensorNone/dispersionTensorNone.C
@@ -0,0 +1,82 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dispersionTensorNone.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvc.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace dispersionTensorModels
+{
+    defineTypeNameAndDebug(dispersionTensorNone, 0);
+
+    addToRunTimeSelectionTable
+    (
+        dispersionTensorModel,
+        dispersionTensorNone,
+        dictionary
+    );
+}
+}
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::dispersionTensorModels::dispersionTensorNone::dispersionTensorNone
+(
+    const fvMesh& mesh,
+    const dictionary& dict
+)
+:
+    dispersionTensorModel(mesh, dict),
+    Deff_
+     (
+      IOobject
+      (
+       "Deff",
+       mesh_.time().timeName(),
+       mesh_,
+       IOobject::READ_IF_PRESENT,
+       IOobject::NO_WRITE
+      ),
+      mesh_,
+      dimensionedTensor("Deff",dimensionSet(0, 2, -1, 0, 0),tensor::zero),
+      "zeroGradient"
+    )
+{
+}
+
+// * * * * * * * * * * * * * * member functions  * * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volTensorField>
+Foam::dispersionTensorModels::dispersionTensorNone::effectiveDispersionTensor() const
+{
+      return Deff_;
+}
+
+void Foam::dispersionTensorModels::dispersionTensorNone::updateDispersionTensor()
+{}
+// -------------------------------------------------------------------------//
